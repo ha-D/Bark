b42c07bce1abea4519663e15f838ed8a1f298373
Mahdi <mahdi.malvandi@pushe.co>
2019-07-07 11:20:18 +0430

    Fixed clearLogs on mainThread

--- bark/src/main/java/sh/hadi/bark/ui/LogViewModel.kt
+++ bark/src/main/java/sh/hadi/bark/ui/LogViewModel.kt
@@ -7,11 +7,9 @@ import sh.hadi.bark.db.LogItem
 
 class LogViewModel(private val dataSource: LogDao) : ViewModel() {
 
-    fun addLog(log: LogItem) {
-        dataSource.insertLog(log)
-    }
+    fun addLog(log: LogItem) = Thread { dataSource.insertLog(log) }.start()
 
     fun getLogs(): LiveData<List<LogItem>> = dataSource.getLogs()
 
-    fun clearLogs() = dataSource.deleteAlLogs()
+    fun clearLogs() = Thread{ dataSource.deleteAlLogs() }.start()
 }
